9d512fb5bc5b60ac9f8de0d9f3497e9cb723f2fd
nashcft <nashrfv@gmail.com>
2017-12-12 01:43:03 +0900

    "Very first user interface"

--- app/src/main/java/com/rap/nashcft/rap/MainActivity.kt
+++ app/src/main/java/com/rap/nashcft/rap/MainActivity.kt
@@ -2,11 +2,23 @@ package com.rap.nashcft.rap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.hello_world_salute)
+    lateinit var helloText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+
+        Observable.just("Hello! Please use this app responsibly!")
+                .subscribe { helloText.text = it }
     }
 }
